5f34e8ca9802f1e43cdb7f3cba2903d00b70ea2f
E126406Y <E126406Y@gitlab.univ-nantes.fr>
2017-12-04 18:06:36 +0100

    Début d'implémentation Attachment et Binding

--- src/m2/elts_architecturaux/Attachment.java
+++ src/m2/elts_architecturaux/Attachment.java
@@ -1,5 +1,12 @@
 package m2.elts_architecturaux;
 
-public class Attachment {
+import m2.composant.Composant;
+import m2.connecteur.Connecteur;
 
+public class Attachment {
+	
+	private Composant composant;
+	private Connecteur connect;
+	
+	
 }
--- src/m2/elts_architecturaux/Binding.java
+++ src/m2/elts_architecturaux/Binding.java
@@ -1,5 +1,11 @@
 package m2.elts_architecturaux;
 
+import m2.composant.Composant;
+import m2.configuration.Configuration;
+
 public class Binding {
 
+	private Composant composant;
+	private Configuration config;
+	
 }
